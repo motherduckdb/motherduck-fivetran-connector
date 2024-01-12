e7050c6c8f3409ee064566b7b61201300d7e816e
Elena Felder <41136058+elefeint@users.noreply.github.com>
2024-01-12 08:02:36 -0800

    put single line in block

--- src/decryption.cpp
+++ src/decryption.cpp
@@ -16,16 +16,19 @@ std::vector<unsigned char> decrypt_file(const std::string &filename,
     throw std::runtime_error("Could not initialize decryption context");
   }
   if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, decryption_key,
-                              iv.data()))
+                              iv.data())) {
     throw std::runtime_error("Could not decrypt file " + filename);
+  }
   int len;
   std::vector<unsigned char> plaintext(encrypted_data.size());
   if (1 != EVP_DecryptUpdate(ctx, plaintext.data(), &len, encrypted_data.data(),
-                             encrypted_data.size()))
+                             encrypted_data.size())) {
     throw std::runtime_error("Could not decrypt UPDATE file " + filename);
+  }
   int plaintext_len = len;
-  if (1 != EVP_DecryptFinal_ex(ctx, plaintext.data() + len, &len))
+  if (1 != EVP_DecryptFinal_ex(ctx, plaintext.data() + len, &len)) {
     throw std::runtime_error("Could not decrypt FINAL file " + filename);
+  }
   plaintext_len += len;
   EVP_CIPHER_CTX_free(ctx);
 
