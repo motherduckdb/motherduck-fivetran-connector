d76bbe08355107f545e39f6c094875538fe199e5
Florian <florian@motherduck.com>
2025-10-17 14:31:09 +0200

    Introduce openssl_helper

--- includes/decryption.hpp
+++ includes/decryption.hpp
@@ -1,6 +1,8 @@
-#include <openssl/evp.h>
 #include <string>
 #include <vector>
 
+std::vector<unsigned char> decrypt_stream(std::istream &input,
+                                          const std::string &filename,
+                                          const unsigned char *decryption_key);
 std::vector<unsigned char> decrypt_file(const std::string &filename,
                                         const unsigned char *decryption_key);
--- src/decryption.cpp
+++ src/decryption.cpp
@@ -1,38 +1,63 @@
-#include <decryption.hpp>
+#include "decryption.hpp"
+#include "openssl_helper.hpp"
+
 #include <fstream>
+#include <openssl/evp.h>
 
-std::vector<unsigned char> decrypt_file(const std::string &filename,
-                                        const unsigned char *decryption_key) {
+/// Decrypts the provided file using AES-256-CBC with PKCS5 padding.
+/// The `filename` parameter is used to provide additional context in error
+/// messages. Returns the plaintext as a vector of bytes.
+std::vector<unsigned char> decrypt_stream(std::istream &input,
+                                          const std::string &filename,
+                                          const unsigned char *decryption_key) {
+  // https://github.com/fivetran/fivetran_partner_sdk/blob/2f13d37849cc866ab71704158f5e9ba247b755b5/development-guide/destination-connector-development-guide.md#encryption
+  // "Each batch file is encrypted separately using AES-256 in CBC mode and with
+  // PKCS5Padding. You can find the encryption key for each batch file in the
+  // WriteBatchRequest#keys field. First 16 bytes of each batch file hold the
+  // IV vector."
 
-  std::ifstream file(filename, std::ios::binary);
+  constexpr int iv_length = 16;
   std::vector<unsigned char> iv(16);
-  file.read(reinterpret_cast<char *>(iv.data()), iv.size());
+  input.read(reinterpret_cast<char *>(iv.data()), iv_length);
 
-  std::vector<unsigned char> encrypted_data(
-      std::istreambuf_iterator<char>{file}, {});
+  const std::vector<unsigned char> encrypted_data(
+      std::istreambuf_iterator<char>{input}, {});
 
   EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
   if (!ctx) {
-    throw std::runtime_error("Could not initialize decryption context");
+    openssl_helper::raise_openssl_error(
+        "Failed to create decryption cipher context");
   }
-  if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, decryption_key,
+  openssl_helper::CipherCtxDeleter ctx_deleter(ctx);
+
+  if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, decryption_key,
                               iv.data())) {
-    throw std::runtime_error("Could not decrypt file " + filename);
+    openssl_helper::raise_openssl_error(
+        "Failed to initialize decryption context for file " + filename);
   }
   int len = 0;
   std::vector<unsigned char> plaintext(encrypted_data.size());
   if (1 != EVP_DecryptUpdate(ctx, plaintext.data(), &len, encrypted_data.data(),
                              encrypted_data.size())) {
-    throw std::runtime_error("Could not decrypt UPDATE file " + filename);
+    openssl_helper::raise_openssl_error("Could not decrypt UPDATE file " +
+                                        filename);
   }
   int plaintext_len = len;
   if (1 != EVP_DecryptFinal_ex(ctx, plaintext.data() + len, &len)) {
-    throw std::runtime_error("Could not decrypt FINAL file " + filename);
+    openssl_helper::raise_openssl_error(
+        "Could not finalize decryption of file " + filename);
   }
   plaintext_len += len;
-  EVP_CIPHER_CTX_free(ctx);
 
   plaintext.resize(plaintext_len);
 
   return plaintext;
+}
+
+/// Decrypts the provided file using AES-256-CBC with PKCS5 padding.
+/// Returns the plaintext as a vector of bytes.
+std::vector<unsigned char> decrypt_file(const std::string &filename,
+                                        const unsigned char *decryption_key) {
+  std::ifstream file(filename, std::ios::binary);
+  return decrypt_stream(file, filename, decryption_key);
 }
