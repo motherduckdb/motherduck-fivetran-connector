991d1c0f90395261abbe8de113159b89a0374a6a
Florian <florian@motherduck.com>
2025-10-20 15:13:53 +0200

    Use unsigned; renaming

--- test/integration/test_server.cpp
+++ test/integration/test_server.cpp
@@ -1068,10 +1068,10 @@ TEST_CASE("Parallel WriteBatch requests", "[integration][write-batch]") {
   auto token = std::getenv("motherduck_token");
   REQUIRE(token);
 
-  constexpr int num_tables = 5;
+  constexpr unsigned int num_tables = 5;
   std::vector<std::string> table_names;
 
-  for (int i = 0; i < num_tables; i++) {
+  for (unsigned int i = 0; i < num_tables; i++) {
     const std::string table_name = "parallel_books_" + std::to_string(i);
     table_names.push_back(table_name);
 
@@ -1089,7 +1089,7 @@ TEST_CASE("Parallel WriteBatch requests", "[integration][write-batch]") {
   // Launch parallel WriteBatch requests that each write to their own table
   std::vector<std::future<grpc::Status>> futures;
 
-  for (int i = 0; i < num_tables; i++) {
+  for (unsigned int i = 0; i < num_tables; i++) {
     futures.push_back(
         std::async(std::launch::async, [&service, &table_names, i, token]() {
           ::fivetran_sdk::v2::WriteBatchRequest request;
@@ -1129,8 +1129,8 @@ TEST_CASE("Parallel DescribeTable requests", "[integration][describe-table]") {
   constexpr unsigned int requests_per_table = 5;
   std::vector<std::string> table_names;
 
-  for (int i = 0; i < num_tables; i++) {
-    const std::string table_name = "parallel_describe_" + std::to_string(i);
+  for (unsigned int t = 0; t < num_tables; t++) {
+    const std::string table_name = "parallel_describe_" + std::to_string(t);
     table_names.push_back(table_name);
 
     ::fivetran_sdk::v2::CreateTableRequest request;
@@ -1146,15 +1146,15 @@ TEST_CASE("Parallel DescribeTable requests", "[integration][describe-table]") {
 
   std::vector<std::future<grpc::Status>> futures;
 
-  for (int i = 0; i < num_tables; i++) {
-    for (int j = 0; j < requests_per_table; j++) {
+  for (unsigned int t = 0; t < num_tables; t++) {
+    for (unsigned int r = 0; r < requests_per_table; r++) {
       futures.push_back(
-          std::async(std::launch::async, [&service, &table_names, i, token]() {
+          std::async(std::launch::async, [&service, &table_names, t, token]() {
             ::fivetran_sdk::v2::DescribeTableRequest request;
             (*request.mutable_configuration())["motherduck_token"] = token;
             (*request.mutable_configuration())["motherduck_database"] =
                 TEST_DATABASE_NAME;
-            request.set_table_name(table_names[i]);
+            request.set_table_name(table_names[t]);
 
             ::fivetran_sdk::v2::DescribeTableResponse response;
             return service.DescribeTable(nullptr, &request, &response);
