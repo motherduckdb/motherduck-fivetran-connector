ac1aad0819324f4c5105bde2d26d66f7b8507ff0
Florian <gerlinghoff@startmail.com>
2025-10-20 18:15:09 +0200

    OpenSSL error messges in exceptions & CIPHER_CTX_free (#82)
    - This PR introduces a helper function `raise_openssl_error` which
    throws a `std::runtime_error` that includes the error messages from the
    thread's OpenSSL error queue. To be fair, these error messages are very
    technical, but if we ever need them, I hope that they can provide some
    additional context.
    - The `openssl_helper` also contains a `CipherCtxDeleter` which makes
    sure that the `EVP_CIPHER_CTX` is cleaned up with `EVP_CIPHER_CTX_free`
    if the function returns early, for example in case of an exception.
    Previously, `EVP_CIPHER_CTX_free` was only called at the end.
    - I added a new `decrypt_stream` function which takes an arbitrary
    `std::istream` to make the function easier testable because it can also
    read from a memory stream (see #83 ). `decrypt_file` now simply creates
    a file stream and hands it over to `decrypt_stream`.
    
    The general decryption implementation is not changed.

---
+++ includes/openssl_helper.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#define OPENSSL_API_COMPAT 30000
+#define OPENSSL_NO_DEPRECATED
+
+#include <openssl/evp.h>
+#include <string>
+
+namespace openssl_helper {
+void raise_openssl_error(const std::string &message_prefix);
+
+/// RAII helper to free EVP_CIPHER and EVP_CIPHER_CTX
+struct CipherCtxDeleter {
+  EVP_CIPHER_CTX *cipher_ctx = nullptr;
+
+  explicit CipherCtxDeleter(EVP_CIPHER_CTX *_cipher_ctx)
+      : cipher_ctx(_cipher_ctx) {}
+
+  ~CipherCtxDeleter() {
+    if (cipher_ctx) {
+      EVP_CIPHER_CTX_free(cipher_ctx);
+    }
+  }
+};
+} // namespace openssl_helper
