474a4095334def1a524076994bd5f5f4934953f1
Florian <florian@motherduck.com>
2025-10-20 15:20:56 +0200

    Rename filename to input_name

--- includes/decryption.hpp
+++ includes/decryption.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 std::vector<unsigned char> decrypt_stream(std::istream &input,
-                                          const std::string &filename,
+                                          const std::string &input_name,
                                           const unsigned char *decryption_key);
 std::vector<unsigned char> decrypt_file(const std::string &filename,
                                         const unsigned char *decryption_key);
