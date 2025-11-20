ccff188dd7dc35a5971cd822e5d56296372521ca
Florian <florian@motherduck.com>
2025-11-20 10:07:51 +0100

    Fix booleans.csv

--- test/test_process_file.cpp
+++ test/test_process_file.cpp
@@ -59,30 +59,23 @@ TEST_CASE("Test reading various CSV files", "[csv_processor]") {
   duckdb::DuckDB db(nullptr);
   duckdb::Connection con(db);
 
+  CAPTURE(filename);
+
   IngestProperties props(test_file.string(), "", {}, "", 1);
   auto logger = std::make_shared<mdlog::MdLog>();
   csv_processor::ProcessFile(
       con, props, logger, [&con, &test_file](const std::string &view_name) {
-        // $1 is the name of the CSVView, $2 is the name of the CSV file
+        // Find the difference between the view and the original CSV data; should be empty
         const std::string compare_query =
-            "WITH expected_data AS (FROM read_csv_auto($2, header = true)) "
-            "(FROM $1 EXCEPT FROM $2) "
+            "WITH expected_data AS (FROM read_csv_auto('" + test_file.string() + "', header = true)) "
+            "(FROM " + view_name + " EXCEPT FROM expected_data) "
             "UNION ALL "
-            "(FROM $2 EXCEPT FROM $1)";
-        auto compare_prepared_stmt = con.Prepare(compare_query);
-        if (compare_prepared_stmt->HasError()) {
-          FAIL("Failed to prepare comparison query: " +
-               compare_prepared_stmt->GetError());
-        }
-        duckdb::vector<duckdb::Value> params = {view_name, test_file.string()};
-        auto compare_result = compare_prepared_stmt->Execute(params, false);
+            "(FROM expected_data EXCEPT FROM " + view_name + ")";
+        const auto compare_result = con.Query(compare_query);
         if (compare_result->HasError()) {
           FAIL("Failed to execute comparison query: " +
                compare_result->GetError());
         }
-        REQUIRE(compare_result->type ==
-                duckdb::QueryResultType::MATERIALIZED_RESULT);
-        REQUIRE(compare_result->Cast<duckdb::MaterializedQueryResult>()
-                    .RowCount() == 0);
+        CHECK(compare_result->RowCount() == 0);
       });
 }
