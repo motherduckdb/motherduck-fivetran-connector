10ecb2ee445b6decd899c7ecbff72f6caf97b99a
Florian <gerlinghoff@startmail.com>
2025-12-05 17:49:06 +0100

    Add class for temporary memory-backed file on Linux (#106)
    Adds a `MemoryBackedFile` class which holds an open file descriptor. The
    memory-backed files will be used to store the decrypted CSV content in.
    The file is unlinked shortly after creation, hence cannot be found in
    the file system and is deleted once the file descriptor is released.
    Because the file is unlinked, we access it via the path `/dev/fd/N`. The
    file descriptor is closed when a `MemoryBackedFile` goes out of scope.
    The file is completely in memory on Linux. On macOS, the file lives in
    the `/tmp` directory.

---
+++ includes/memory_backed_file.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+/// A RAM-backed file on Linux. On macOS, this file is located in the /tmp
+/// directory. It is not visible in the filesystem, but accessible via its file
+/// descriptor.
+class MemoryBackedFile {
+public:
+  [[nodiscard]] static MemoryBackedFile Create(size_t file_size);
+
+  ~MemoryBackedFile();
+
+  MemoryBackedFile(const MemoryBackedFile &) = delete;
+  MemoryBackedFile &operator=(const MemoryBackedFile &) = delete;
+  MemoryBackedFile(MemoryBackedFile &&other) noexcept;
+  MemoryBackedFile &operator=(MemoryBackedFile &&other) noexcept;
+
+  int fd;
+  // On BSD/macOS, the cursor is shared between file descriptors
+  // (https://man.freebsd.org/cgi/man.cgi?fdescfs): "if the file descriptor is
+  // open and the mode the file is being opened with is a subset of the
+  // mode of the existing descriptor, the call: `fd = open("/dev/fd/0",
+  // mode);` and the call: `fd = fcntl(0, F_DUPFD, 0);` are equivalent."
+  std::string path;
+
+private:
+  // The file descriptor can be accessed via /dev/fd/<fd> on both Linux and
+  // macOS
+  explicit MemoryBackedFile(const int fd_)
+      : fd(fd_), path("/dev/fd/" + std::to_string(fd_)) {}
+};
