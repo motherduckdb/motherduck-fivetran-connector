91943837d35d7b65c0f8c5320cc9301eac85b411
Florian <florian@motherduck.com>
2025-12-05 11:42:38 +0100

    Test fixes

--- test/test_process_file.cpp
+++ test/test_process_file.cpp
@@ -32,8 +32,7 @@ TEST_CASE("Test can read simple CSV file", "[csv_processor]") {
   auto logger = std::make_shared<mdlog::MdLog>();
   csv_processor::ProcessFile(
       con, props, logger, [&con](const std::string &view_name) {
-        const std::string query = "FROM " + view_name;
-        auto res = con.Query(query);
+        const auto res = con.Query("FROM " + view_name);
         REQUIRE_FALSE(res->HasError());
         REQUIRE(res->ColumnCount() == 3);
         REQUIRE(res->RowCount() == 3);
@@ -78,8 +77,7 @@ TEST_CASE("Test reading CSV file with auto-detection of column types", "[csv_pro
     auto logger = std::make_shared<mdlog::MdLog>();
     csv_processor::ProcessFile(
         con, props, logger, [&con](const std::string &view_name) {
-          const std::string query = "FROM " + view_name;
-          auto res = con.Query(query);
+          const auto res = con.Query("FROM " + view_name);
           REQUIRE_FALSE(res->HasError());
           REQUIRE(res->ColumnCount() == 3);
           CHECK(res->RowCount() == 3);
@@ -92,11 +90,42 @@ TEST_CASE("Test reading CSV file with auto-detection of column types", "[csv_pro
         });
 }
 
+TEST_CASE("Test reading CSV file with columns out of order", "[csv_processor]") {
+    const fs::path test_file =
+        fs::path(TEST_RESOURCES_DIR) / "csv" / "small_simple.csv";
+    CAPTURE(test_file);
+    REQUIRE(fs::exists(test_file));
+
+    duckdb::DuckDB db(nullptr);
+    duckdb::Connection con(db);
+
+    // In the CSV file, the order is id, name, age
+    std::vector<column_def> columns {
+        column_def { .name = "name", .type = duckdb::LogicalType::VARCHAR },
+        column_def { .name = "age", .type = duckdb::LogicalType::SMALLINT }
+        column_def { .name = "id", .type = duckdb::LogicalType::INTEGER },
+    };
+    IngestProperties props(test_file.string(), "", columns, "", 1);
+    auto logger = std::make_shared<mdlog::MdLog>();
+    csv_processor::ProcessFile(
+        con, props, logger, [&con](const std::string &view_name) {
+          const std::string query = "FROM " + view_name + " ORDER BY id LIMIT 1";
+          auto res = con.Query(query);
+          REQUIRE_FALSE(res->HasError());
+          REQUIRE(res->ColumnCount() == 3);
+          CHECK(res->RowCount() == 1);
+
+          REQUIRE(res->GetValue(0, 0).ToString() == "Alice");
+          REQUIRE(res->GetValue(1, 0).GetValue<int>() == 30);
+          REQUIRE(res->GetValue(2, 0).GetValue<int>() == 1);
+        });
+}
+
 
 // TODO: Test cases:
-// - Test column order not matching
 // - Test with different column data types (e.g. different int types). Also edge cases like overflow
 // - various error paths
+// - CSV block size
 
 TEST_CASE("Test reading various CSV files", "[csv_processor]") {
     auto [filename, row_count, columns] = GENERATE(table<std::string, size_t, std::vector<column_def>>({
@@ -222,12 +251,12 @@ TEST_CASE("Test reading various CSV files", "[csv_processor]") {
   auto logger = std::make_shared<mdlog::MdLog>();
   csv_processor::ProcessFile(
       con, props, logger, [&con, expected_row_count = row_count](const std::string &view_name) {
-        const auto read_csv_result = con.Query("FROM " + view_name);
-        if (read_csv_result->HasError()) {
+        const auto res = con.Query("FROM " + view_name);
+        if (res->HasError()) {
           FAIL("Failed to execute comparison query: " +
-               read_csv_result->GetError());
+               res->GetError());
         }
-        CHECK(read_csv_result->RowCount() == expected_row_count);
+        CHECK(res->RowCount() == expected_row_count);
       });
 }
 
@@ -250,8 +279,7 @@ TEST_CASE("Test reading CSV file with special null string", "[csv_processor]") {
     auto logger = std::make_shared<mdlog::MdLog>();
     csv_processor::ProcessFile(
         con, props, logger, [&con](const std::string &view_name) {
-            const std::string query = "FROM " + view_name;
-            auto res = con.Query(query);
+            const auto res = con.Query("FROM " + view_name);
             REQUIRE_FALSE(res->HasError());
             REQUIRE(res->ColumnCount() == 4);
             REQUIRE(res->RowCount() == 2);
@@ -291,8 +319,7 @@ TEST_CASE("Test reading CSV file with unmodified string setting", "[csv_processo
     auto logger = std::make_shared<mdlog::MdLog>();
     csv_processor::ProcessFile(
         con, props, logger, [&con](const std::string &view_name) {
-            const std::string query = "FROM " + view_name;
-            auto res = con.Query(query);
+            const auto res = con.Query("FROM " + view_name);
             REQUIRE_FALSE(res->HasError());
             REQUIRE(res->ColumnCount() == 3);
             REQUIRE(res->RowCount() == 2);
@@ -316,8 +343,29 @@ TEST_CASE("Test reading CSV file with unmodified string setting", "[csv_processo
 }
 
 TEST_CASE("Test reading zstd-compressed CSV files", "[csv_processor]") {
-  const auto filename =
-      GENERATE("customers-100000.csv.zst", "train_few_shot.csv.zst");
+    auto [filename, row_count, columns] = GENERATE(table<std::string, size_t, std::vector<column_def>>({
+        std::make_tuple<std::string, size_t, std::vector<column_def>>("customers-100000.csv.zst", 100000, {
+            { .name = "Index", .type = duckdb::LogicalTypeId::INTEGER },
+            { .name = "Customer Id", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "First Name", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "Last Name", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "Company", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "City", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "Country", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "Phone 1", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "Phone 2", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "Email", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "Subscription Date", .type = duckdb::LogicalTypeId::DATE },
+            { .name = "Website", .type = duckdb::LogicalTypeId::DATE }
+        }),
+        std::make_tuple<std::string, size_t, std::vector<column_def>>("train_few_shot.csv.zst", 40000, {
+            { .name = "text", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "label", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "source", .type = duckdb::LogicalTypeId::VARCHAR },
+            { .name = "reasoning", .type = duckdb::LogicalTypeId::VARCHAR }
+        })
+    }));
+
   const fs::path test_file =
       fs::path(TEST_RESOURCES_DIR) / "compressed_csv" / filename;
   CAPTURE(test_file);
@@ -326,26 +374,13 @@ TEST_CASE("Test reading zstd-compressed CSV files", "[csv_processor]") {
   duckdb::DuckDB db(nullptr);
   duckdb::Connection con(db);
 
-  IngestProperties props(test_file.string(), "", {}, "", 1);
+  IngestProperties props(test_file.string(), "", columns, "", 1);
   auto logger = std::make_shared<mdlog::MdLog>();
   csv_processor::ProcessFile(
-      con, props, logger, [&con, &test_file](const std::string &view_name) {
-        // Find the difference between the view and the original CSV data;
-        // should be empty
-        const std::string compare_query =
-            "WITH expected_data AS (FROM read_csv('" + test_file.string() +
-            "', header=true, encoding='utf-8', compression='zstd')) "
-            "(FROM " +
-            view_name +
-            " EXCEPT FROM expected_data) "
-            "UNION ALL "
-            "(FROM expected_data EXCEPT FROM " +
-            view_name + ")";
-        const auto compare_result = con.Query(compare_query);
-        if (compare_result->HasError()) {
-          FAIL("Failed to execute 2 query: " + compare_result->GetError());
-        }
-        CHECK(compare_result->RowCount() == 0);
+      con, props, logger, [&con, expected_row_count = row_count](const std::string &view_name) {
+            const auto res = con.Query("FROM " + view_name);
+            REQUIRE_FALSE(res->HasError());
+            REQUIRE(res->RowCount() == expected_row_count);
       });
 }
 
