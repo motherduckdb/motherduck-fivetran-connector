c98a3e2f714c4330bd7f33b5bef18c51816ce345
Florian <florian@motherduck.com>
2026-01-28 14:27:05 +0100

    Trigger WelcomePackFetch when creating DuckDB instance

--- src/request_context.cpp
+++ src/request_context.cpp
@@ -9,7 +9,7 @@ RequestContext::RequestContext(
     const std::string &endpoint_name_, ConnectionFactory &connection_factory,
     const google::protobuf::Map<std::string, std::string> &request_config)
     : endpoint_name(endpoint_name_),
-      con(connection_factory.GetConnection(
+      con(connection_factory.CreateConnection(
           config::find_property(request_config, config::PROP_TOKEN),
           config::find_property(request_config, config::PROP_DATABASE))),
       logger(mdlog::Logger::CreateMultiSinkLogger(&con)) {
