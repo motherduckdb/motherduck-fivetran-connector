5dae784a67de6a6c0769e4209dd334daf1e923b3
Donny Peeters <46660228+Donnype@users.noreply.github.com>
2026-02-10 16:54:08 +0100

    Make max_line_size configurable (#142)
    Make the `max_line_size` configurable through the configuration form. I
    decided to use MiB as a unit since I believe that's enough granularity
    while keeping the value "readable". We now return a task instead of an
    error when we detect that the reading of the CSV failed on the line
    size.

--- includes/config.hpp
+++ includes/config.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <optional>
 #include <stdexcept>
 #include <string>
 
 namespace config {
 inline constexpr const char* PROP_DATABASE = "motherduck_database";
 inline constexpr const char* PROP_TOKEN = "motherduck_token";
+inline constexpr const char* PROP_MAX_RECORD_SIZE = "max_record_size";
 
 template <typename MapLike>
 std::string find_property(const MapLike& config, const std::string& property_name) {
@@ -14,5 +16,14 @@ std::string find_property(const MapLike& config, const std::string& property_nam
 		throw std::invalid_argument("Missing property " + property_name);
 	}
 	return token_it->second;
+}
+
+template <typename MapLike>
+std::optional<std::string> find_optional_property(const MapLike& config, const std::string& property_name) {
+	const auto token_it = config.find(property_name);
+	if (token_it == config.end()) {
+		return std::nullopt;
+	}
+	return std::make_optional(token_it->second);
 }
 } // namespace config
