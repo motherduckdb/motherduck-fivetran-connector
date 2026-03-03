b94af34cb313173cf9992ef6185646973744d350
Florian <florian@motherduck.com>
2026-03-03 17:04:55 +0100

    Use sigaction

--- src/motherduck_destination.cpp
+++ src/motherduck_destination.cpp
@@ -1,9 +1,9 @@
 #include "extension_helper.hpp"
 #include "motherduck_destination_server.hpp"
 
-#include <csignal>
 #include <execinfo.h>
 #include <grpcpp/grpcpp.h>
+#include <signal.h>
 #include <string>
 #include <unistd.h>
 
@@ -23,7 +23,7 @@ void RunServer(const std::string& port) {
 	server->Wait();
 }
 
-void logCrash(const int sig) {
+void log_crash(const int sig) {
 	constexpr char msg[] = "\n=== SIGSEGV or SIGABRT ===\nStack trace:\n";
 	write(STDERR_FILENO, msg, sizeof(msg) - 1);
 
@@ -35,13 +35,19 @@ void logCrash(const int sig) {
 	const int num_frames = backtrace(callstack, MAX_DEPTH);
 	backtrace_symbols_fd(callstack, num_frames, STDERR_FILENO);
 
-	signal(sig, SIG_DFL);
 	raise(sig);
 }
 
 int main(const int argc, char** argv) {
-	std::signal(SIGSEGV, logCrash);
-	std::signal(SIGABRT, logCrash);
+	struct sigaction sa = {};
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = log_crash;
+	// SA_RESETHAND resets the signal handler to the default. If log_crash itself segfaults, then the default handler
+	// (coredump) is triggered. SA_NODEFER makes sure that if log_crash itself segfaults, the process dies immediately
+	// instead of hanging until log_crash is finished.
+	sa.sa_flags = static_cast<int>(SA_RESETHAND | SA_NODEFER);
+	sigaction(SIGSEGV, &sa, nullptr);
+	sigaction(SIGABRT, &sa, nullptr);
 
 	std::string port = "50052";
 	for (auto i = 1; i < argc; i++) {
